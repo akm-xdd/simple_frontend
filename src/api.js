fee9f1e0d66d3cf7fa5a52bbbd8dfd85f849a018
Anand Mishra <ctfu.anand@outlook.com>
2025-05-22 16:07:09 +0530

    fix: correct baseURL syntax in axios instance

--- src/api.js
+++ src/api.js
@@ -2,6 +2,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://4.247.144.132/:8000/api/items/',
+  baseURL: 'http://4.247.144.132:8000/api/items/',
   headers: { 'Content-Type': 'application/json' },
 });
